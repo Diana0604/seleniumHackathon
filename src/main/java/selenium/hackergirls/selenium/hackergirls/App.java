bd0929fcae112d41c322e4fea83ef7c39433e297
Diana0604 <diana.vallverdu@gmail.com>
2017-03-04 06:24:54 +0100

    Enters paypal with the user you give it. Trying to create interfaace

---
+++ src/main/java/selenium/hackergirls/selenium/hackergirls/App.java
@@ -0,0 +1,13 @@
+package selenium.hackergirls.selenium.hackergirls;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
