a52456d7bbad4826895d88f7e82aee9ff577d669
Diana0604 <diana.vallverdu@gmail.com>
2017-03-04 07:00:23 +0100

    super libs

--- src/main/java/Prova1/entrarPayPal.java
+++ src/main/java/Prova1/entrarPayPal.java
@@ -2,13 +2,10 @@ package Prova1;
 
 import java.util.Scanner;
 
-import org.openqa.selenium.By; 
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement; 
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.chrome.ChromeDriver; 
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class entrarPayPal {
 
